4cf3cdeab58deec1de16e493163340e12ac2a39f
Tom Fogal <tfogal@alumni.unh.edu>
2012-03-26 09:23:58 -0600

    Don't hardcode path to matlab.
    Also, remove old/unnecessary parts of the LD_PRELOAD garbage

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-#LD_PRELOAD=/usr/lib64/libstdc++.so.6:/usr/lib64/libQtCore.so.4.7.1:/usr/lib64/libQtGui.so.4.7.1:/usr/lib64/libQtOpenGL.so.4.7.1 \
-#
 LD_PRELOAD=/usr/lib64/libstdc++.so.6 \
-  /usr/sci/local/matlab/bin/matlab ./uvf.mexa64
+  matlab ./uvf.mexa64
